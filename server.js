5f92bccdca20d4ada5ad8452c59c33dfc9933d3c
danukaw <danuka.wijetunge@ebuilder.com>
2018-01-26 18:11:17 +0530

    node webserver

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=> {
+    let now = new Date().toString();
+    let log = `http request method is ${req.method} and path ${req.url} time : ${now}`;
+    fs.appendFile('server.log', log + '\n',(err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('update.hbs');
+});
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=> {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle : 'Home Page ...',
+        welcomeMessage : 'Welcome to my website'
+    });
+});
+
+app.get('/about', (request, response) => {
+    //response.send('<h4>About page</h4>');
+    response.render('about.hbs', {
+        pageTitle : 'About Page...',
+    });
+});
+
+app.get('/error', (req, res) => {
+    res.send({
+        error : {
+            errorCode : 404,
+            errorMessage : "Page cannot be found"
+        }
+    })
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
